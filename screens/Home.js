a5150b45826060dab0d0e0d5262bbeeee833c392
MarioDanielCeronMayoral <92552033+MarioDanielCeronMayoral@users.noreply.github.com>
2023-05-03 19:49:19 -0600

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import {Text, View} from "react";
+
+export default class HomeScreen extends Component {
+    render () {
+        return (
+            <View style = {{
+                flex:1,
+                justifyContent:"center",
+                alignItems:"center",
+            }}>
+                <Text>Pantalla de Inicio</Text>
+            </View>
+        ) 
+    }
+}
+
+
+
+
+
+
---
+++ screens/IssLocation.js
@@ -0,0 +1,24 @@
+import React,{Component} from "react"
+import {Text, View} from "react"
+
+export default class IssLocation extends Component {
+    render () {
+        return (
+            <View style = {{
+                flex:1,
+                justifyContent:"center",
+                alignItems:"center",
+
+
+            }}>
+                <Text>Pantalla de Localizacion</Text>
+            </View>
+        ) 
+    }
+}
+
+
+
+
+
+
